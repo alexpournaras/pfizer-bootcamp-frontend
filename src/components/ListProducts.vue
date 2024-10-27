a341d64eb3e8bf7deefed011ef06d06cf072dfdf
greenpainog <vaggelis139915@gmail.com>
2024-10-27 19:40:37 +0200

    add product me error handling se batch name , name kai date .batch number kai name prepei na nai uinqie kai tha dixnei analogo message se periptwsh lathous.to date prepei na exei exp_date>func_date

--- src/components/ListProducts.vue
+++ src/components/ListProducts.vue
@@ -5,9 +5,88 @@
                 <h2 class="title">Products Management</h2>
             </v-col>
 
-            <v-col cols="12" class="text-center">
-                <v-btn prepend-icon="mdi-plus-circle" color="primary" @click="goToAddPage">Add Product</v-btn>
+             <!-- Add Product Button that activates the modal -->
+             <v-col cols="12" class="text-center">
+                <v-btn prepend-icon="mdi-plus-circle" color="primary" @click="dialog = true">Add Product</v-btn>
             </v-col>
+
+             <!-- Modal for adding a new product -->
+             <v-dialog v-model="dialog" max-width="600">
+                <v-card>
+                    <v-card-title class="headline">Add Product</v-card-title>
+
+                    <v-card-text>
+                        <v-form @submit.prevent="submitProduct">
+                            <v-text-field v-model="product.name" label="Product Name" required></v-text-field>
+                            
+                            <v-select
+                                v-model="product.category"
+                                :items="['Capsule', 'Tablet', 'Injection']"
+                                label="Category*"
+                                required
+                                >
+                            
+                            </v-select>
+
+                            <v-text-field
+                                v-model="product.active_ingredients"
+                                label="Active Ingredients (comma-separated)"
+                                required
+                            ></v-text-field>
+
+                            <v-text-field 
+                            v-model="product.batch_number" 
+                            label="Batch Number"
+                            required>
+
+                            </v-text-field>
+                            <v-select
+                                v-model="product.research_status"
+                                :items="['Completed', 'Under Development', 'In clinical Trials']"
+                                label="Research Status"
+                                return-object
+                                required>
+                            ></v-select>
+                            <v-text-field
+                                v-model="product.manufacturing_date"
+                                label="Manufacturing Date"
+                                type="date"
+                                required
+                            ></v-text-field>
+                            <v-text-field
+                                v-model="product.expiration_date"
+                                label="Expiration Date"
+                                type="date"
+                                required
+                            ></v-text-field>
+                        </v-form>
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn text @click="dialog = false">Close</v-btn>
+                        <v-btn color="primary" @click="submitProduct">Submit</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+
+            <v-snackbar
+                v-model="snackbar"
+                multi-line
+                :color="snackbarColor" 
+            >
+                {{ snackbarText }}
+
+                <template v-slot:actions>
+                <v-btn
+                    color="white"
+                    variant="text"
+                    @click="snackbar = false"
+                >
+                    Close
+                </v-btn>
+                </template>
+            </v-snackbar>
 
             <v-col cols="12" sm="12" class="mx-auto">
                 <v-text-field v-model="searchQuery" append-inner-icon="mdi-magnify" density="compact"
@@ -57,11 +136,12 @@
 </template>
 
 <script>
-import { getProducts, deleteProduct } from "../services/productService.js";
+import { getProducts, deleteProduct,createProduct  } from "../services/productService.js";
 
 export default {
     data() {
         return {
+            dialog: false,  // Controls the dialog visibility
             itemsPerPage: 10,
             headers: [
                 { title: "ID", key: "id", sortable: true },
@@ -75,6 +155,20 @@ export default {
             totalItems: 0,
             searchQuery: "",
             loading: true,
+            product: {  // Holds the form data
+                name: '',
+                category: '',
+                active_ingredients: '',
+                batch_number: '',
+                research_status: '',
+                manufacturing_date: '',
+                expiration_date: '',
+            },
+            snackbar: false,          // Snackbar visibility
+            snackbarText: '',        // Snackbar message
+            snackbarColor: 'green',  // Snackbar color
+            selectedCategory: '',   
+            selectedResearchStatus: '',  
         };
     },
     mounted() {
@@ -100,9 +194,66 @@ export default {
 
             this.loading = false;
         },
-        goToAddPage() {
-            this.$router.push({ name: "AddProduct" });
+
+        async submitProduct() {
+            try {
+                const productData = {
+                    name: this.product.name,
+                    category: this.product.category, // Make sure this is bound correctly
+                    active_ingredients: this.product.active_ingredients.split(',').map(ingredient => ingredient.trim()), // Convert to array
+                    batch_number: this.product.batch_number,
+                    research_status: this.product.research_status, // Ensure this is bound correctly
+                    manufacturing_date: this.product.manufacturing_date,
+                    expiration_date: this.product.expiration_date,
+                };
+
+                console.log('Submitting product data:', productData); // Add this line
+
+                await createProduct(productData);
+
+                // Close the modal and reload the product list if successful
+                this.dialog = false;
+                this.loadItems({ page: 1, itemsPerPage: this.itemsPerPage });
+                this.resetForm();
+                // Show success snackbar
+                this.snackbarText = 'Submission successful!';
+                this.snackbarColor = 'green';
+                this.snackbar = true;
+            } catch (error) {
+
+                console.error('Error adding product:', error.message);
+                if (error.response && error.response.status === 422) {
+                    const validationErrors = error.response.data.errors;
+
+                    if (validationErrors.name) {
+                        this.snackbarText = 'Product name already exists.';
+                    } else if (validationErrors.batch_number) {
+                        this.snackbarText = 'Batch number already exists.Batch number needs to be unique';
+                    } 
+                    else if (error.response.data.message === 'The expiration date must be after the manufacturing date.') {
+                        this.snackbarText = 'The expiration date must be after the manufacturing date.';
+                    } else {
+                        this.snackbarText = "Couldn't submit the data.";
+                    }
+
+                    this.snackbarColor = 'red';
+                    this.snackbar = true;
+                    }
+            }
         },
+
+        resetForm() {
+            this.product = {
+                name: '',
+                category: '',
+                active_ingredients: '',
+                batch_number: '',
+                research_status: '',
+                manufacturing_date: '',
+                expiration_date: '',
+            };
+        },
+        
         goToEditPage(productId) {
             this.$router.push({
                 name: "EditProduct",
